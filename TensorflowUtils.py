16f9f99ac45481cd663662f15eb5b66726af1a40
Sarath Shekkizhar <shekkizh@usc.edu>
2016-04-20 18:11:44 -0700

    Upating utils

--- TensorflowUtils.py
+++ TensorflowUtils.py
@@ -60,7 +60,12 @@ def conv2d_strided(x, W, b):
 
 
 def conv2d_transpose_strided(x, W, b):
-    conv = tf.nn.conv2d_transpose(x, W, strides=[1, 2, 2, 1], padding="SAME")
+    output_shape = x.get_shape().as_list()
+    output_shape[1] *=2
+    output_shape[2] *=2
+    output_shape[3] = W.get_shape().as_list()[2]
+    # print output_shape
+    conv = tf.nn.conv2d_transpose(x, W, output_shape, strides=[1, 2, 2, 1], padding="SAME")
     return tf.nn.bias_add(conv, b)
 
 
