4774bb8f0f611a462dd8469f4cc14c6354431e0a
charlie-linux-pc <shekkizh@usc.edu>
2016-09-03 12:52:19 -0700

    testing dropout pruning

--- Model_Pruning/OptimalBrainDamage_2.py
+++ Model_Pruning/OptimalBrainDamage_2.py
@@ -17,16 +17,17 @@ FLAGS = tf.flags.FLAGS
 tf.flags.DEFINE_integer("batch_size", "64", "batch size for training")
 tf.flags.DEFINE_string("logs_dir", "logs/MNIST_Pruning_logs/", "path to logs directory")
 tf.flags.DEFINE_string("data_dir", "Data_zoo/MNIST_data/", "path to dataset")
-tf.flags.DEFINE_float("learning_rate", "1e-4", "Learning rate for Adam Optimizer")
+tf.flags.DEFINE_float("learning_rate", "1e-5", "Learning rate for Adam Optimizer")
 
 MAX_ITERATIONS = int(5e4 + 1)
 IMAGE_SIZE = 28
 
 
-def inference(data):
+def inference(data, keep_prob):
     with tf.variable_scope("inference") as scope:
         weight_variable_size = IMAGE_SIZE * IMAGE_SIZE * 50 + 50 * 50 * 3 + 50 * 10
         bias_variable_size = 4 * 50 + 10
+        print (weight_variable_size + bias_variable_size)
         variable = utils.weight_variable([weight_variable_size + bias_variable_size], name="variables")
         weight_variable = tf.slice(variable, [0], [weight_variable_size], name="weights")
         bias_variable = tf.slice(variable, [weight_variable_size], [bias_variable_size], name="biases")
@@ -34,7 +35,8 @@ def inference(data):
         bias_offset = 0
         W_1 = tf.slice(weight_variable, [weight_offset], [IMAGE_SIZE * IMAGE_SIZE * 50], name="W_1")
         b_1 = tf.slice(bias_variable, [bias_offset], [50], name="b_1")
-        h_1 = tf.nn.relu(tf.matmul(data, tf.reshape(W_1, [IMAGE_SIZE * IMAGE_SIZE, 50])) + b_1, name='h_1')
+        h_1_relu = tf.nn.relu(tf.matmul(data, tf.reshape(W_1, [IMAGE_SIZE * IMAGE_SIZE, 50])) + b_1, name='h_1')
+        h_1 = tf.nn.dropout(h_1_relu, keep_prob)
         utils.add_activation_summary(h_1)
 
         weight_offset += IMAGE_SIZE * IMAGE_SIZE * 50
@@ -42,7 +44,8 @@ def inference(data):
 
         W_2 = tf.slice(weight_variable, [weight_offset], [50 * 50], name="W_2")
         b_2 = tf.slice(bias_variable, [bias_offset], [50], name="b_2")
-        h_2 = tf.nn.relu(tf.matmul(h_1, tf.reshape(W_2, [50, 50])) + b_2, name='h_2')
+        h_2_relu = tf.nn.relu(tf.matmul(h_1, tf.reshape(W_2, [50, 50])) + b_2, name='h_2')
+        h_2 = tf.nn.dropout(h_2_relu, keep_prob)
         utils.add_activation_summary(h_2)
 
         weight_offset += 50 * 50
@@ -50,7 +53,8 @@ def inference(data):
 
         W_3 = tf.slice(weight_variable, [weight_offset], [50 * 50], name="W_3")
         b_3 = tf.slice(bias_variable, [bias_offset], [50], name="b_3")
-        h_3 = tf.nn.relu(tf.matmul(h_2, tf.reshape(W_3, [50, 50])) + b_3, name='h_3')
+        h_3_relu = tf.nn.relu(tf.matmul(h_2, tf.reshape(W_3, [50, 50])) + b_3, name='h_3')
+        h_3 = tf.nn.dropout(h_3_relu, keep_prob)
         utils.add_activation_summary(h_3)
 
         weight_offset += 50 * 50
@@ -58,7 +62,8 @@ def inference(data):
 
         W_4 = tf.slice(weight_variable, [weight_offset], [50 * 50], name="W_4")
         b_4 = tf.slice(bias_variable, [bias_offset], [50], name="b_4")
-        h_4 = tf.nn.relu(tf.matmul(h_3, tf.reshape(W_4, [50, 50])) + b_4, name='h_4')
+        h_4_relu = tf.nn.relu(tf.matmul(h_3, tf.reshape(W_4, [50, 50])) + b_4, name='h_4')
+        h_4 = tf.nn.dropout(h_4_relu, keep_prob)
         utils.add_activation_summary(h_4)
 
         weight_offset += 50 * 50
@@ -88,13 +93,14 @@ def train(loss, var_list):
 def main(argv=None):
     input_data = tf.placeholder(tf.float32, [None, 784])
     truth_labels = tf.placeholder(tf.float32, [None, 10])
+    keep_prob = tf.placeholder(tf.float32)
     dataset = mnist.input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-    pred_labels = inference(input_data)
+    pred_labels = inference(input_data, keep_prob)
     entropy = -tf.reduce_sum(truth_labels * tf.log(pred_labels))
     tf.scalar_summary('Cross_entropy', entropy)
     train_vars = tf.trainable_variables()
-    for v in train_vars:
-        utils.add_to_regularization_and_summary(v)
+    # for v in train_vars:
+    #     utils.add_to_regularization_and_summary(v)
     train_op, hess = train(entropy, train_vars)
     accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pred_labels, 1), tf.argmax(truth_labels, 1)), tf.float32))
 
@@ -106,7 +112,8 @@ def main(argv=None):
     summary_op = tf.merge_all_summaries()
 
     def test():
-        test_accuracy = accuracy.eval(feed_dict={input_data: dataset.test.images, truth_labels: dataset.test.labels})
+        test_accuracy = accuracy.eval(
+            feed_dict={input_data: dataset.test.images, truth_labels: dataset.test.labels, keep_prob: 1.0})
         return test_accuracy
 
     ckpt = tf.train.get_checkpoint_state(FLAGS.logs_dir)
@@ -116,7 +123,7 @@ def main(argv=None):
     else:
         for i in xrange(MAX_ITERATIONS):
             batch = dataset.train.next_batch(FLAGS.batch_size)
-            feed_dict = {input_data: batch[0], truth_labels: batch[1]}
+            feed_dict = {input_data: batch[0], truth_labels: batch[1], keep_prob: 0.8}
             train_op.run(feed_dict=feed_dict)
 
             if i % 10 == 0:
@@ -152,7 +159,7 @@ def main(argv=None):
 
     for count in range(1, 8):
         batch = dataset.train.next_batch(FLAGS.batch_size)
-        feed_dict = {input_data: batch[0], truth_labels: batch[1], k: count}
+        feed_dict = {input_data: batch[0], truth_labels: batch[1], k: count, keep_prob: 1.0}
         sess.run(scatter_update_op, feed_dict=feed_dict)
         print('Variables Perecent: %d, Test accuracy: %g' % (10*(10 - count), test()))
         sess.run(scatter_restore_op, feed_dict=feed_dict)
